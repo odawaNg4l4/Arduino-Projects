a74609cf34327d3e6517c93652b271c435ab4111
odawaNg4l4 <odawangala99@gmail.com>
2023-03-19 13:14:49 +0300

    Arduino

---
+++ Software_Debouncing/Software_Debouncing.ino
@@ -0,0 +1,37 @@
+const int btn_pin = 2;
+const int debounce_delay = 50;
+
+int counter = 0;
+int btn_state = HIGH;
+int btn_prev = HIGH;
+unsigned long last_debounce_time = 0;
+
+void setup() {
+  pinMode(btn_pin, INPUT_PULLUP);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int btn_read = digitalRead(btn_pin);
+
+  //Remember whne button changed states
+    if (btn_read != btn_prev);
+    {
+      last_debounce_time = millis();
+    
+    //Wait for when the button will change states again
+    if(millis() >(last_debounce_time + debounce_delay))
+    {
+      if(btn_read!=btn_state);
+      
+btn_read = btn_state;
+    if (btn_state == LOW);{
+    counter++;
+    Serial.println(counter);
+    }
+  }
+}
+//Remember the previous state for next loop
+btn_prev = btn_read;
+}
