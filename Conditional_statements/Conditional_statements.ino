9b341db3ff4aaa0afc2c7bd339a6b1b3d39d1181
odawaNg4l4 <odawangala99@gmail.com>
2023-03-22 11:02:15 +0300

    Arduino

---
+++ Conditional_statements/Conditional_statements.ino
@@ -0,0 +1,18 @@
+int a = 5;
+
+void setup() {
+  Serial.begin(9600);
+    if(a < 10){
+      Serial.print("I AM HAPPY");
+    }
+  else{
+    Serial.println("I AM SAD");
+  }
+    }
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
