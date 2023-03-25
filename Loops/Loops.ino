20bda0e8dafcd45f3b2a0e31cc0bdbe4374be798
odawaNg4l4 <odawangala99@gmail.com>
2023-03-25 19:34:15 +0300

    Arduino projects

--- Loops/Loops.ino
+++ Loops/Loops.ino
@@ -1,14 +1,17 @@
-int i = 4;
+int i = 3;
+int j = 0;
 
 void setup() {
   Serial.begin(9600);
 }
 void loop(){
-  if(i < 3){
-       while(i < 5){
+  if (i < 3){
+       while(j < 5){
     
        Serial.println("ODAWA");
+       j++;
   }
   }
+  // Serial.println("ODAWA");
   delay(1000);
 }
