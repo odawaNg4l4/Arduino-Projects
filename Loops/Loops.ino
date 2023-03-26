8f1f8a8b55dd0fe825153d6c0c96e47779f9669f
odawaNg4l4 <odawangala99@gmail.com>
2023-03-26 16:28:57 +0300

    Arduino

--- Loops/Loops.ino
+++ Loops/Loops.ino
@@ -12,6 +12,43 @@ void loop(){
        j++;
   }
   }
+  }
   // Serial.println("ODAWA");
   delay(1000);
+}
+//FOR LOOP
+int a;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    for(a > 0; a <= 10; a++)
+Serial.println(a);
+
+delay(1000);
+}
+
+//COMPUTE SUM OF 10 NATURAL NUMBERS
+int a;
+int result=0;
+
+void setup()
+{
+Serial.begin(9600);
+ for(a = 1;a <= 10;a++)
+  {
+    result = result + a;
+    Serial.println(a);
+  }
+  Serial.println(result);
+  delay(1000);
+}
+
+void loop()
+{
+
 }
